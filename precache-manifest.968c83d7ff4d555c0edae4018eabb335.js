1dfaaa84133ca34449e8daf264bfceb4feaffa88
Pradeep Kushwaha <prk.shine@gmail.com>
2020-04-25 19:54:01 +0530

    Updates

---
+++ precache-manifest.968c83d7ff4d555c0edae4018eabb335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811dac4e68fcb9dbe12b333b0b7cc937",
+    "url": "/monster-mania/index.html"
+  },
+  {
+    "revision": "b3fe7a7743d8113eba88",
+    "url": "/monster-mania/static/css/main.149add41.chunk.css"
+  },
+  {
+    "revision": "4020e150d1bfd3fae371",
+    "url": "/monster-mania/static/js/2.665414ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-mania/static/js/2.665414ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3fe7a7743d8113eba88",
+    "url": "/monster-mania/static/js/main.305980f4.chunk.js"
+  },
+  {
+    "revision": "ff9b5976e5ee2d12f8f4",
+    "url": "/monster-mania/static/js/runtime-main.355eb0cc.js"
+  }
+]);
